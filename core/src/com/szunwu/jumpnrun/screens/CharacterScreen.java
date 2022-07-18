f5ca34c6458f2af801be24965fb0467f2e0a9bc2
Xelilali <svenja.diringer@gmx.de>
2022-07-18 19:42:17 +0200

    comment test

--- core/src/com/szunwu/jumpnrun/screens/CharacterScreen.java
+++ core/src/com/szunwu/jumpnrun/screens/CharacterScreen.java
@@ -1,18 +1,50 @@
 package com.szunwu.jumpnrun.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.szunwu.jumpnrun.GameMain;
 
 public class CharacterScreen implements Screen {
 
     private GameMain game;
 
+    private Stage stage;
+
+    private Label b;
+    private Label g;
+    private CheckBox boy;
+    private CheckBox girl;
+
     public CharacterScreen(GameMain gameMain){
         game = gameMain;
+
+        // add Stage with Viewport as argument -> controller for user input
+        stage = new Stage(new ScreenViewport());
+
+        // tell Stage to act and draw -> without no actions after user input/no content
+        Gdx.input.setInputProcessor(stage);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
     }
 
     @Override
     public void show() {
+
+        //create table that fills the screen -> everything later is added to the table
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+        stage.addActor(table);
+
+
+        //set skin for buttons
+        Skin skin = new Skin(Gdx.files.internal("glassy/skin/glassy-ui.json"));
 
     }
 
